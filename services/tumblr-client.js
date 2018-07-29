19e7060ae217f94894f452cda4a484b0bdf54f4b
t-skgm <skgm0301@gmail.com>
2018-07-29 10:56:45 +0900

    Dotenvの読み込みタイミングエラー

--- services/tumblr-client.js
+++ services/tumblr-client.js
@@ -1,12 +1,13 @@
 import tumblr from 'tumblr.js'
-import { env } from './dotenv'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const client = tumblr.createClient({
   credentials: {
-    consumer_key:    env.TUMBLR_CONSUMER_KEY,
-    consumer_secret: env.TUMBLR_CONSUMER_SECRET,
-    token:           env.TUMBLR_TOKEN,
-    token_secret:    env.TUMBLR_TOKEN_SECRET
+    consumer_key:    process.env.TUMBLR_CONSUMER_KEY,
+    consumer_secret: process.env.TUMBLR_CONSUMER_SECRET,
+    token:           process.env.TUMBLR_TOKEN,
+    token_secret:    process.env.TUMBLR_TOKEN_SECRET
   },
   returnPromises: true
 })
